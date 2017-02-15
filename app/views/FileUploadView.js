3f7e41dbcd5f0efb20fec7357943258bf3a3119b
Michael Prather <mprather1@gmail.com>
2017-02-15 06:21:51 +0000

    please select a file

--- app/views/FileUploadView.js
+++ app/views/FileUploadView.js
@@ -18,38 +18,41 @@ var FileUploadView = Backbone.Marionette.View.extend({
     
     var that = this;
     
-    $('.progress-bar').css('width', '0%')    
-    $('.progress').removeClass('hide');
-    
-    var file = document.getElementById('file-upload').files[0]
-    var newFile = new File({ file_name: file.name, file_size: file.size, mimetype: file.type })
-    
-    var formData = new FormData();
-    formData.append('upload', file)
-    
-    var xhr = new XMLHttpRequest();
-    
-    xhr.open('post', '/api/files', true)
-    
-    xhr.upload.onprogress = function(e){
-      if(e.lengthComputable){
-        var percentage = (e.loaded / e.total) * 100
-        $('.progress-bar').css('width', percentage + '%')
-      }
-    }
-    
-    xhr.onerror = function(e){
-      that.showInfo("Error...")
-    }
-    
-    xhr.onload = function(){
-      that.showInfo(file.name + " was successfully uploaded...")
-      that.collection.add(newFile)
-      document.getElementById('file-upload').value = ''
-    }
-    
-    xhr.send(formData)
 
+    var file = document.getElementById('file-upload').files[0]
+
+    if(file !== undefined){
+      $('.progress-bar').css('width', '0%')    
+      $('.progress').removeClass('hide');
+      var newFile = new File({ file_name: file.name, file_size: file.size, mimetype: file.type })
+    
+      var formData = new FormData();
+      formData.append('upload', file)
+      var xhr = new XMLHttpRequest();
+    
+      xhr.open('post', '/api/files', true)
+    
+      xhr.upload.onprogress = function(e){
+        if(e.lengthComputable){
+          var percentage = (e.loaded / e.total) * 100
+          $('.progress-bar').css('width', percentage + '%')
+        }
+      }
+    
+      xhr.onerror = function(e){
+        that.showInfo("Error...")
+      }
+      
+      xhr.onload = function(){
+        that.showInfo(file.name + " was successfully uploaded...")
+        that.collection.add(newFile)
+        document.getElementById('file-upload').value = ''
+      }
+      
+      xhr.send(formData)
+    } else {
+      that.showInfo("Please select a file...")
+    }
   }
 });
 
